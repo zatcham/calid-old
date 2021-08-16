68e602522a4fac65c7208e14399954512495c155
zatcham <37421850+zatcham@users.noreply.github.com>
2021-08-16 21:12:01 +0100

    Login now shows log out message, and started work on login redirect
    - Need ot work out best way to get the last page and pass to login so user can be redirected to last page (is this even needed, maybe not in mvp)

--- web/public/auth/login.php
+++ web/public/auth/login.php
@@ -8,8 +8,7 @@ require $document_root . '\newdir\include\classes\Auth.php';
 
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
-
-//
+use UAParser\Parser;
 
 // twig init
 $loader = new FilesystemLoader('../../templates');
@@ -18,6 +17,25 @@ $twig = new Environment($loader);
 $username = $password = "";
 $username_error = $password_error = $login_error = "";
 $login_success = "";
+
+// gets page to redirect to, if none go to dashboard
+if (isset($_GET)) {
+    if (!empty($_GET)) {
+        if (isset($_GET['msg'])) {
+            if (!empty(trim($_GET['msg']))) {
+                if (trim($_GET['msg']) == "autologout") {
+                    $login_success = "You were logged out due to inactivity.";
+                } elseif (trim($_GET['msg']) == "logout") {
+                    $login_success = "You were logged out successfully!";
+                }
+            }
+        }
+    } else {
+        $page_redirect = "";
+    }
+} else {
+    $page_redirect = "";
+}
 
 // if form submitted, run this code
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -53,15 +71,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                 $_SESSION["loggedin"] = true; // password is correct
                                 $_SESSION["id"] = $id;
                                 $_SESSION["username"] = $username;
-                                Auth::addLoginAttempt($id, ($_SERVER['REMOTE_ADDR']), "Success"); // adds login attempt to db
+                                $parser = Parser::create();
+                                $user_agent = $parser->parse($_SERVER['HTTP_USER_AGENT'])->toString(); // TODO: error handling if null
+                                Auth::addLoginAttempt($id, ($_SERVER['REMOTE_ADDR']), "Success", $user_agent); // adds login attempt to db
                                 $login_success = "Login success, redirecting...";
                                 $stmt->close();
-                                // Redirect user to welcome page
-                                header("location: ../home.php");
+                                // Redirect user to specified page, or dashboard
+                                if ($page_redirect == "") {
+                                    header("location: ../home.php");
+                                } else { // no page, go to dashboard
+                                    header("location: ../$page_redirect");
+                                }
+
                             } else {
                                 // Password is incorrect
                                 $login_error = "Invalid username or password.";
-                                Auth::addLoginAttempt($id, ($_SERVER['REMOTE_ADDR']), "Fail");
+                                $parser = Parser::create();
+                                $user_agent = $parser->parse($_SERVER['HTTP_USER_AGENT'])->toString(); // TODO: error handling if null
+                                Auth::addLoginAttempt($id, ($_SERVER['REMOTE_ADDR']), "Fail", $user_agent);
                             }
                         }
                     } else {
--- web/public/auth/logout.php
+++ web/public/auth/logout.php
@@ -2,13 +2,26 @@
 // Initialize the session
 session_start();
 
+// Get page to redirect to
+//if (isset($_COOKIE['last_page'])) {
+//    $last_page = $_COOKIE['last_page'];
+//}
+
 // Unset all of the session variables
 $_SESSION = array();
 
 // Destroy the session.
 session_destroy();
 
-// Redirect to login page
-header("location: login.php");
+if (isset($_GET)) {
+    if (!empty($_GET['msg'])) {
+        if (trim($_GET['msg']) == "autologout") {
+            header("location: login.php?msg=autologout");
+        }
+    }
+}
+
+// If nothing, go to login and show logout message
+header("location: login.php?msg=logout");
 exit;
 ?>
